256d85fa8b20ec9959e942d26f0f73eef1e5026c
S. Vivek Iyer <46563529+iyer99iyer@users.noreply.github.com>
2022-04-28 00:16:14 +0530

    text to speech added

--- lib/ui/topic_learning/topic_learning_view.dart
+++ lib/ui/topic_learning/topic_learning_view.dart
@@ -1,28 +1,43 @@
 import 'package:ai_friend/ui/topic_learning/topic_learning_viewmodel.dart';
+import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 
 class TopicLearningView extends StatelessWidget {
-
   final String topicName;
 
-  const TopicLearningView({Key? key, required this.topicName}) : super(key: key);
+  const TopicLearningView({Key? key, required this.topicName})
+      : super(key: key);
   @override
   Widget build(BuildContext context) {
     return ViewModelBuilder<TopicLearningViewModel>.reactive(
         onModelReady: (model) => model.init(),
         builder: (context, model, child) => Scaffold(
-            appBar: AppBar(
-              title: Text(topicName),
-            ),
-            body:Container(
-                ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: (){},
-            child: Icon(Icons.mic),
-          ),
-          floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+              appBar: AppBar(
+                title: Text(topicName),
               ),
+              body: SingleChildScrollView(
+                reverse: true,
+                child: Container(
+                  padding: const EdgeInsets.fromLTRB(30, 30, 30, 150),
+                    child: Text(model.text)
+                ),
+              ),
+              floatingActionButton: AvatarGlow(
+                animate: model.isListening,
+                glowColor: Theme.of(context).primaryColor,
+                endRadius: 75.0,
+                duration: const Duration(milliseconds: 2000),
+                repeatPauseDuration: const Duration(milliseconds: 100),
+                repeat: true,
+                child: FloatingActionButton(
+                  onPressed: ()=>model.listen(),
+                  child: Icon(Icons.mic),
+                ),
+              ),
+              floatingActionButtonLocation:
+                  FloatingActionButtonLocation.centerDocked,
+            ),
         viewModelBuilder: () => TopicLearningViewModel());
   }
 }
--- lib/ui/topic_learning/topic_learning_viewmodel.dart
+++ lib/ui/topic_learning/topic_learning_viewmodel.dart
@@ -1,20 +1,50 @@
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
 
 import '../../app/app.locator.dart';
 import '../../app/app.router.dart';
 
-class TopicLearningViewModel extends BaseViewModel{
-
+class TopicLearningViewModel extends BaseViewModel {
   final _navigationService = locator<NavigationService>();
 
+  late stt.SpeechToText _speech;
+  bool _isListening = false;
+  String _text = 'Press the button and start speaking';
+  double _confidence = 1.0;
 
+  bool get isListening => _isListening;
+  String get text => _text;
+  double get confidence => _confidence;
 
-  Future init() async{
-
+  Future init() async {
+    _speech = stt.SpeechToText();
   }
 
-  void doSomething(){
-    _navigationService.navigateTo(Routes.dashboardView);
+  listen() async {
+    if (!_isListening) {
+      bool available = await _speech.initialize(
+          onStatus: (val) {
+            print('onStatus: $val');
+            if (val == 'done') _isListening = false;
+            notifyListeners();
+          },
+          onError: (val) => print('onError: $val'));
+      if (available) {
+        _isListening = true;
+        notifyListeners();
+        _speech.listen(onResult: (val) {
+          _text = val.recognizedWords;
+          if (val.hasConfidenceRating && val.confidence > 0) {
+            _confidence = val.confidence;
+          }
+          notifyListeners();
+        });
+      }
+    } else {
+      _isListening = false;
+      notifyListeners();
+      _speech.stop();
+    }
   }
 }
